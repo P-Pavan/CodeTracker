52826a59814072e4fa089ed1e2f82d42b456285c
Suryab02 <bsprabhas2002@gmail.com>
2023-10-21 17:14:43 +0530

    display flex prob creating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+// import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
 
 const App = () => {
   return (
-    <div>{document.title}</div>
+    <div>
+      <Login />
+    </div>
   )
 }
 
---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './home.module.css';
+function Home() {
+  return <div>
+    <h1>Welocme to the home page</h1>
+  </div>;
+}
+
+export default Home;
